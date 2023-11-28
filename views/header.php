1e1451d7c797e8d8f8cdfd2d8692290c81ea2e85
sawsaw45 <sawyer.2002@hotmail.com>
2023-11-28 13:41:00 -0600

    ugh

--- views/header.php
+++ views/header.php
@@ -6,7 +6,6 @@
     <title><?=$pageTitle?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
 </head>
 <body>
 
