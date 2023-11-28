ef3427bdb08f4558f9b374269c2b595c0979a345
sawsaw45 <sawyer.2002@hotmail.com>
2023-11-28 13:33:47 -0600

    mas teseting

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 require_once("models/connection.php");
 require_once("models/index-db.php");
-$users = getTestIndex();
+//$users = getTestIndex();
 $login = login('sawyer', 'password2!');
 $pageTitle = "Home";
 include "views/header.php";
 ?>
-    <h1>Final Project <?php echo $_SESSION['user_id']?></h1>
+    //<h1>Final Project <?php ?></h1>
 <div class="table-responsive">
     <table class="table table-striped table-sm">
         <thead>
