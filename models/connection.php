5e1fd79c7c92f0658b9deb7928b4d99fa00177d9
sawsaw45 <sawyer.2002@hotmail.com>
2023-11-27 22:08:29 -0600

    idk

--- models/connection.php
+++ models/connection.php
@@ -1,6 +1,6 @@
 <?php
 function get_db_connection(){
-    $conn = new mysqli('159.89.47.44', 'sawyerha_FinalDBUser', '=;#VNe}=4eXu', 'sawyerha_FinalDB');
+    $conn = new mysqli('11.110.0.15', 'sawyerha_FinalDBUser', '=;#VNe}=4eXu', 'sawyerha_FinalDB');
 
     if ($conn->connect_error) {
         return false;
