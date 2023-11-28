a3994032ff4fdc6a175e22f35461ab7049e763f6
sawsaw45 <sawyer.2002@hotmail.com>
2023-11-27 22:14:33 -0600

    brain damage

--- models/index-db.php
+++ models/index-db.php
@@ -2,7 +2,7 @@
 function getTestIndex() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT username, password FROM users");
+        $stmt = $conn->prepare("SELECT username, password FROM user");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
