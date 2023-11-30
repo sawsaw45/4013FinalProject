52777fc4343149eec2fb76183cd2f20bc100ae4d
sawsaw45 <sawyer.2002@hotmail.com>
2023-11-29 22:33:00 -0600

    try again?

--- index.php
+++ index.php
@@ -7,7 +7,10 @@
     <title>To-Doo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <?php $loggedin = false;
+    <?php
+    require_once("models/connection.php");
+    require_once("models/index-db.php");
+    $loggedin = false;
     $loggedin = $_SESSION['logged_in'];?>
 </head>
 <body>
@@ -50,13 +53,7 @@
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
-<?php
-require_once("models/connection.php");
-require_once("models/index-db.php");
 
-
-
-?>
     <h1>Final Project <?php ?></h1>
 
 <form method="post" action="index.php">
