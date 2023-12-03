ecfdd256e6111d4ffb74a367ae09fad822e5d8bf
sawsaw45 <sawyer.2002@hotmail.com>
2023-12-02 22:49:43 -0600

    testing

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>To-Doo</title>
+    <link rel="stylesheet" href="customstyles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <?php
@@ -47,7 +48,7 @@
     foreach ($notes as $note) {
         ?> <div class="col">
     <?php
-        echo '<div class="card">';
+        echo '<div class="card cards">';
         echo '<div class="card-body">';
         echo '<h5 class="card-title">' . $note['Name'] . '</h5>';
         echo '<p class="card-text">' . $note['Contents'] . '</p>';
