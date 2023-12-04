e758ea9ff7d35488a6dd3b1dd5553b5328339617
sawsaw45 <sawyer.2002@hotmail.com>
2023-12-04 10:45:18 -0600

    fix logic

--- view-index.php
+++ view-index.php
@@ -114,7 +114,7 @@
     }
 
     // Get all cards and initialize countdown for each
-    var cards = document.querySelectorAll('.col-md');
+    var cards = document.querySelectorAll('.card-body');
     cards.forEach(function(card) {
         var dueDate = card.dataset.dueDate;
         initializeCardCountdown(card, dueDate);
