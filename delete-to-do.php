2e273da9a3d04065ce8786866917b27ab15547f5
sawsaw45 <sawyer.2002@hotmail.com>
2023-12-03 22:31:40 -0600

    sillygooese

--- delete-to-do.php
+++ delete-to-do.php
@@ -1,5 +1,5 @@
 <form method="post" action="">
-    <input type="hidden" name="cid" value="<?php echo $note['noteid']; ?>">
+    <input type="hidden" name="noteid" value="<?php echo $note['noteid']; ?>">
     <input type="hidden" name="actionType" value="Delete">
     <button type="submit" class="btn btn-primary" onclick="return confirm('ARE YOU SURE ABOUT THAT?!?!?!');">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
