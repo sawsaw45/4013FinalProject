9d71d07263134eb12b3ff477a6ddea82f02b71c3
sawsaw45 <sawyer.2002@hotmail.com>
2023-12-04 10:57:56 -0600

    test test

--- view-index.php
+++ view-index.php
@@ -66,9 +66,9 @@
 
         ?> <div class="col-md mb-2 d-flex align-items-stretch">
 
-       <div class="card border <?php echo $priorityBg?>">
+       <div class="card border <?php echo $priorityBg?>" date-due-date="<?php echo $note['Due Date']?>">
            
-       <div class="card-body d-flex flex-column" date-due-date="<?php echo $note['Due Date']?>">
+       <div class="card-body d-flex flex-column" >
        <h5 class="card-title"><?php echo $note['Name']?></h5>
        <p class="card-text d-inline-block text-truncate"><?php echo $note['Contents']?></p>
        <p class="card-text"><strong>Due Date:</strong> <?php echo $note['Due Date']?></p>
@@ -114,7 +114,7 @@
     }
 
     // Get all cards and initialize countdown for each
-    var cards = document.querySelectorAll('.card-body');
+    var cards = document.querySelectorAll('.col-md .card');
     cards.forEach(function(card) {
         var cardId = card.id.replace("card-", ""); // Extract the noteid from the card ID
         var dueDate = card.dataset.dueDate;
